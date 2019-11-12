145809581d082aa69524c064216c1d05e80ba9dc
bcudina <bozidar.cudina@gmail.com>
2019-11-12 22:49:21 +0100

    Update omis.ino

--- omis.ino
+++ omis.ino
@@ -2,7 +2,8 @@ void setup()
 {
   // put your setup code here, to run once:
   pinMode(2, OUTPUT); //dfs
-    digitalWrite(2, HIGH);
+  digitalWrite(2, HIGH);
+  //pocetak ***************************
   delay(1000);
   digitalWrite(2, LOW);
   delay(1000);
@@ -42,21 +43,9 @@ void setup()
   delay(1000);
   digitalWrite(2, LOW);
   delay(1000);
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(2, LOW);
-  delay(1000);
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(2, LOW);
-  delay(1000);
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(2, LOW);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-
 }
